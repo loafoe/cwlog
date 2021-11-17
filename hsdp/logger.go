9521d4aee9945ea53e640e76fe225401a4d11e19
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-11-17 06:35:30 +0100

    Generate CustomLogEvent

--- hsdp/logger.go
+++ hsdp/logger.go
@@ -6,20 +6,18 @@ import (
 	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
 )
 
-func StartLogger() (chan cloudwatchlogs.OutputLogEvent, chan bool, error) {
-	logChan := make(chan  cloudwatchlogs.OutputLogEvent)
+func StartLogger(group, stream string) (chan cloudwatchlogs.OutputLogEvent, chan bool, error) {
+	logChan := make(chan cloudwatchlogs.OutputLogEvent)
 	doneChan := make(chan bool)
-
-
 
 	go func() {
 		for {
 			select {
-				case msg := <- logChan:
-					fmt.Printf("%d: %s\n", msg.Timestamp, *msg.Message)
-				case <- doneChan:
-					fmt.Printf("exiting logger\n")
-					return
+			case msg := <-logChan:
+				fmt.Printf("CustomLogEvent|%s|%s|%s|%s|%s\n", "1", "2", "3", stream, *msg.Message)
+			case <-doneChan:
+				fmt.Printf("exiting logger\n")
+				return
 			}
 		}
 	}()
--- main.go
+++ main.go
@@ -1,118 +1,124 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "time"
+	"flag"
+	"fmt"
+	"time"
 
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/cloudwatchlogs"
-    "github.com/philips-software/cwlog/hsdp"
-    "github.com/spf13/pflag"
-    "github.com/spf13/viper"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
+	"github.com/philips-software/cwlog/hsdp"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 )
 
 func GetLogEvents(sess *session.Session, limit int64, nextToken string, logGroupName string, logStreamName string) (*cloudwatchlogs.GetLogEventsOutput, error) {
-    svc := cloudwatchlogs.New(sess)
+	svc := cloudwatchlogs.New(sess)
 
-    input := &cloudwatchlogs.GetLogEventsInput{
-        Limit:         &limit,
-        LogGroupName:  &logGroupName,
-        LogStreamName: &logStreamName,
-    }
-    if nextToken != "" {
-        input.NextToken = &nextToken
-    }
+	input := &cloudwatchlogs.GetLogEventsInput{
+		Limit:         &limit,
+		LogGroupName:  &logGroupName,
+		LogStreamName: &logStreamName,
+	}
+	if nextToken != "" {
+		input.NextToken = &nextToken
+	}
 
-    resp, err := svc.GetLogEvents(input)
-    if err != nil {
-        return nil, err
-    }
+	resp, err := svc.GetLogEvents(input)
+	if err != nil {
+		return nil, err
+	}
 
-    return resp, nil
+	return resp, nil
 }
 
 func ListLogStreams(sess *session.Session, limit int64, logGroupName string) (*cloudwatchlogs.DescribeLogStreamsOutput, error) {
-    svc := cloudwatchlogs.New(sess)
-    resp, err := svc.DescribeLogStreams(&cloudwatchlogs.DescribeLogStreamsInput{
-        Limit: &limit,
-        LogGroupName: &logGroupName,
-    })
-    if err != nil {
-        return nil, err
-    }
-    return resp, nil
+	svc := cloudwatchlogs.New(sess)
+	resp, err := svc.DescribeLogStreams(&cloudwatchlogs.DescribeLogStreamsInput{
+		Limit:        &limit,
+		LogGroupName: &logGroupName,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
 }
 
 func main() {
-    viper.SetEnvPrefix("cwlog")
-    viper.AutomaticEnv()
-    flag.Int64("limit", 50, "The maximum number of events to retrieve")
-    flag.String("group", "", "The name of the log group")
-    flag.String("stream", "", "The name of the log stream")
-    pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
-    pflag.Parse()
-    _ = viper.BindPFlags(pflag.CommandLine)
+	// Configuration
+	viper.SetEnvPrefix("cwlog")
+	viper.AutomaticEnv()
+	flag.Int64("limit", 50, "The maximum number of events to retrieve")
+	flag.String("group", "", "The name of the log group")
+	flag.String("stream", "", "The name of the log stream")
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+	pflag.Parse()
+	_ = viper.BindPFlags(pflag.CommandLine)
 
-    limit := viper.GetInt64("limit")
-    logGroupName := viper.GetString("group")
-    logStreamName := viper.GetString("stream")
+	limit := viper.GetInt64("limit")
+	logGroupName := viper.GetString("group")
+	logStreamName := viper.GetString("stream")
 
-    listStreams := false
+	listStreams := false
 
-    if logGroupName == ""  {
-        fmt.Println("You must supply a log group name (-g LOG-GROUP) and log stream name (-s LOG-STREAM)")
-        return
-    }
-    if logStreamName == "" {
-        fmt.Println("Listing stream names")
-        listStreams = true
-    }
+	if logGroupName == "" {
+		fmt.Println("You must supply a log group name (--group LOG-GROUP) and log stream name (--stream LOG-STREAM)")
+		return
+	}
+	if logStreamName == "" {
+		fmt.Println("Listing stream names")
+		listStreams = true
+	}
 
-    sess := session.Must(session.NewSessionWithOptions(session.Options{
-        SharedConfigState: session.SharedConfigEnable,
-    }))
+	// AWS SDK session init
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
 
-    if listStreams {
-        resp, err :=ListLogStreams(sess, limit, logGroupName)
-        if err != nil {
-            fmt.Println("Got error getting log streams:")
-            fmt.Println(err)
-            return
-        }
-        for _, stream := range resp.LogStreams {
+	// List streams check
+	if listStreams {
+		resp, err := ListLogStreams(sess, limit, logGroupName)
+		if err != nil {
+			fmt.Println("Got error getting log streams:")
+			fmt.Println(err)
+			return
+		}
+		for _, stream := range resp.LogStreams {
 
-            fmt.Println("  ", *stream.LogStreamName)
-        }
-        return
-    }
+			fmt.Println("  ", *stream.LogStreamName)
+		}
+		return
+	}
 
-    gotToken := ""
-    nextToken := ""
+	// Main
+	gotToken := ""
+	nextToken := ""
 
-    fmt.Println("Event messages for stream " + logStreamName + " in log group  " + logGroupName)
+	queue, done, err := hsdp.StartLogger(logGroupName, logStreamName)
+	if err != nil {
+		fmt.Printf("Error setting up HSDP logger: %v\n", err)
+		return
+	}
 
-    queue, done, err := hsdp.StartLogger()
-    if err != nil {
-        fmt.Printf("Error setting up HSDP logger: %v\n", err)
-        return
-    }
+	fmt.Printf("Starting %s:%s -> HSDP Logging\n", logStreamName, logGroupName)
+	for {
+		resp, err := GetLogEvents(sess, limit, nextToken, logGroupName, logStreamName)
+		if err != nil {
+			fmt.Println("Got error getting log events:")
+			fmt.Println(err)
+			break
+		}
+		for _, event := range resp.Events {
+			queue <- *event
+		}
+		gotToken = nextToken
+		nextToken = *resp.NextForwardToken
 
-    for {
-        resp, err := GetLogEvents(sess, limit, nextToken, logGroupName, logStreamName)
-        if err != nil {
-            fmt.Println("Got error getting log events:")
-            fmt.Println(err)
-            break
-        }
-        for _, event := range resp.Events {
-            queue <- *event
-        }
-        gotToken = nextToken
-        nextToken = *resp.NextForwardToken
-        if gotToken == nextToken {
-            time.Sleep(1 * time.Second)
-        }
-    }
-    done <- true
+		// This can be tweaked at some point
+		if gotToken == nextToken {
+			time.Sleep(1 * time.Second)
+		}
+	}
+	// Stop go-routine
+	done <- true
 }
